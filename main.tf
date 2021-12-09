74161d7a7be8e6debb94c149ee0a5ac04e7977dc
rawhideron <rawhideron.gmail.com>
2021-12-09 10:34:37 -0500

    new branch

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "example-org-7ad71a"
 
     workspaces {
-      name = "REPLACE_ME"
+      name = "demo-github-actions"
     }
   }
 }
